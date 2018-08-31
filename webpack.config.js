81ef1b6140a0f4cb0749e35355fdb1af512a9529
Cesar Borge <cborge@konradgroup.com>
2018-08-31 14:41:16 -0600

    React with Webpack and Babel

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    resolve: { extensions: ['*', '.js', '.jsx'] },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            {test: /\.(js)$/, use: 'babel-loader'},
+            {test: /\.css$/, use: ['style-loader', 'css-loader']}
+        ]
+    },
+    mode: 'development', //production
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
