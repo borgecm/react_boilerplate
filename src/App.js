809cb3107309266bf33255059f7c29f8a01db2fa
Cesar Borge <cborge@konradgroup.com>
2018-09-04 10:01:44 -0600

    Reactstrap added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Switch, Route, Link } from 'react-router-dom'
+import { Jumbotron, Button } from 'reactstrap';
 
 //Pages
 import Home from './pages/home/Home'
 import NoHome from './pages/noHome/NoHome'
 
 class App extends React.Component {
-    render(){
-        return(
+    render() {
+        return (
             <div>
+                <Jumbotron>
+                    <h1 className="display-3">Hello, world!</h1>
+                    <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+                    <hr className="my-2" />
+                    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                    <p className="lead">
+                        <Button color="primary">Learn More</Button>
+                    </p>
+                </Jumbotron>
                 <nav>
                     <ul>
                         <li><Link to='/'>Home</Link></li>
@@ -21,7 +31,7 @@ class App extends React.Component {
                         <Route exact path='/' component={Home} />
                         <Route path='/nohome' component={NoHome} />
                     </Switch>
-                </main>                
+                </main>
             </div>
         )
     }
